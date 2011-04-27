91bdb1e9732f964df2258396ed112aca76623cd8
Dan Kang <dan@dskang.com>
2011-04-26 22:31:29 -0400

    Added the folliwng functions to dskang.el: - Match parenthesis using % like vi - Toggle dedicated window - Converted auto-indentation into a loop for specified modes

--- dskang.el
+++ dskang.el
@@ -69,17 +69,9 @@
 ;;   (let (parens-require-spaces)
 ;;      (insert-pair)))
 
-;; Auto-indentation for Python files
-(add-hook 'python-mode-hook '(lambda () (define-key python-mode-map "\C-m" 'reindent-then-newline-and-indent)))
-
-;; Auto-indentation for Java files
-(add-hook 'java-mode-hook '(lambda () (define-key java-mode-map "\C-m" 'reindent-then-newline-and-indent)))
-
-;; Auto-indentation for HTML files
-(add-hook 'html-mode-hook '(lambda () (define-key html-mode-map "\C-m" 'reindent-then-newline-and-indent)))
-
-;; Auto-indentation for PHP files
-(add-hook 'php-mode-hook '(lambda () (define-key php-mode-map "\C-m" 'reindent-then-newline-and-indent)))
+;; Auto-indentation
+(dolist (hook '(c-mode-hook python-mode-hook html-mode-hook php-mode-hook))
+  (add-hook hook '(lambda () (local-set-key "\C-m" 'reindent-then-newline-and-indent))))
 
 ;; Tabs
 (setq-default indent-tabs-mode nil)
@@ -95,3 +87,44 @@
 ;; Use command key as meta
 (setq mac-command-modifier 'meta)
 (setq mac-option-modifier 'nil)
+
+;; Use command key as meta
+(setq mac-command-modifier 'meta)
+(setq mac-option-modifier 'nil)
+
+;; Don't ask for confirmation upon creating new buffer or file
+(setq confirm-nonexistent-file-or-buffer 'nil)
+
+;; Set PATH in emacs shell to be same as Terminal.app for Macs
+;; (defun set-exec-path-from-shell-PATH ()
+;;   (let ((path-from-shell 
+;;          (replace-regexp-in-string "[[:space:]\n]*$" "" 
+;;                                    (shell-command-to-string "$SHELL -l -c 'echo $PATH'"))))
+;;     (setenv "PATH" path-from-shell)
+;;     (setq exec-path (split-string path-from-shell path-separator))))
+;; (when (equal system-type 'darwin) (set-exec-path-from-shell-PATH))
+
+;; Enable ido everywhere
+(ido-everywhere t)
+
+;; Emulates vi so that <%> key shows matching parenthesis
+(global-set-key "%" 'match-paren)
+    (defun match-paren (arg)
+      "Go to the matching paren if on a paren; otherwise insert %."
+      (interactive "p")
+      (cond ((looking-at "\\s\(") (forward-list 1) (backward-char 1))
+            ((looking-at "\\s\)") (forward-char 1) (backward-list 1))
+            (t (self-insert-command (or arg 1)))))
+
+;; Dedicated windows
+(defun toggle-dedicated-window ()
+  "Toggles whether the selected window is dedicated."
+  (interactive)
+  (if (window-dedicated-p (selected-window))
+      (progn
+        (set-window-dedicated-p (selected-window) nil)
+        (minibuffer-message "Window is no longer dedicated."))
+    (progn
+      (set-window-dedicated-p (selected-window) t)
+      (minibuffer-message "Window is now dedicated."))))
+(global-set-key "\C-cd" 'toggle-dedicated-window)
