1a9c62c1032cae97cff68262842a1b5c5d5e8fa0
Dan Kang <dan@dskang.com>
2011-05-14 00:56:15 -0400

    Fixed color theme error in starter kit and added solarized theme

--- dskang.el
+++ dskang.el
@@ -86,8 +86,16 @@
 (setq-default tab-width 4)
 
 ;; Color Theme
+(setq dotfiles-dir (file-name-directory
+                    (or (buffer-file-name) load-file-name)))
+(setq user-specific-dir (concat dotfiles-dir user-login-name))
+(add-to-list 'load-path (concat user-specific-dir "/color-theme"))
 (require 'color-theme)
-(color-theme-zenburn)
+
+;; Use the solarized-dark color theme
+(add-to-list 'load-path (concat user-specific-dir "/color-theme-solarized"))
+(require 'color-theme-solarized)
+(color-theme-solarized-dark)
 
 ;; Tramp
 (setq tramp-default-method "ssh")
