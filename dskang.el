2bacfdd430d017f46ecb488dbb63253a67a85a40
Dan Kang <dan@dskang.com>
2011-08-29 13:21:37 -0700

    Added Growl notifications to ERC

--- dskang.el
+++ dskang.el
@@ -176,6 +176,33 @@ environment."
         ("home" "Going home!")
         ("^$" "")))
 
+;; Growl notifications for ERC
+(defvar growlnotify-command (executable-find "growlnotify") "The path to growlnotify")
+
+(defun growl (title message)
+  "Shows a message through the growl notification system using
+ `growlnotify-command` as the program."
+  (flet ((encfn (s) (encode-coding-string s (keyboard-coding-system))) )
+    (let* ((process (start-process "growlnotify" nil
+                                   growlnotify-command
+                                   (encfn title)
+                                   "-a" "Emacs"
+                                   "-n" "Emacs")))
+      (process-send-string process (encfn message))
+      (process-send-string process "\n")
+      (process-send-eof process)))
+  t)
+
+(defun my-erc-hook (match-type nick message)
+  "Shows a growl notification, when user's nick was mentioned. If the buffer is currently not visible, makes it sticky."
+  (unless (posix-string-match "^\\** *Users on #" message)
+    (growl
+     (concat "ERC: name mentioned on: " (buffer-name (current-buffer)))
+     message
+     )))
+
+(add-hook 'erc-text-matched-hook 'my-erc-hook)
+
 ;; File associations
 (setq auto-mode-alist (cons '("README" . text-mode) auto-mode-alist))
 (add-to-list 'auto-mode-alist '("\\.php$" . php-mode))
@@ -257,4 +284,4 @@ environment."
 (add-hook 'inf-ruby-mode-hook 'echo-false-comint)
 
 ;; Set lisp program to be used for SLIME
-(setq inferior-lisp-program "clisp -K full")
+;; (setq inferior-lisp-program "clisp -K full")
